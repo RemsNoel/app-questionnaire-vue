b081bd6e1a50c6a3f6addfa5c21d51fff44c4917
RemsNoel <remy.noel@epsi.fr>
2019-10-15 17:38:20 +0200

    Enchainement questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -4,18 +4,17 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div> -->
   <div class="Question">
-  	<h2>Question template</h2>
+  	<h2>Questionnaire</h2>
   	<!-- <p>{{ form }}</p> -->
   	<div>
-    <b-form-group>
-    	{{questions[i].ennoncer}}
+    <b-form-group :label="questions[i].ennoncer">    	
       <b-form-radio  v-for="reponse in questions[i].reponses" v-model="selected" name="some-radios" v-bind:value="reponse">{{reponse}}</b-form-radio>
       </b-form-radio-group>
     </b-form-group>
     
 
     <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
-    <b-button type="submit" variant="primary">Question Suivante</b-button>
+    <b-button type="submit" variant="primary" @click="onSubmit">Question Suivante</b-button>
   </div>
 
   <!-- <pre>{{questions}}</pre> -->
@@ -37,13 +36,20 @@ export default {
   },
   created: function(){
   	this.fetchData()
-  	console.log()
 
   },
   methods: {
   	fetchData: function(){
   		this.questions = require("../assets/questions.json");
+
+  	console.log(this.questions)
   	},
+  	onSubmit(evt) {
+  		if (this.i < 11) {
+        this.i++
+    	}
+        // alert(JSON.stringify(this.form))
+     },
   }
 }
 </script>
